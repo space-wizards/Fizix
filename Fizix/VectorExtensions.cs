f10e49c77871417c24bdfb3bb1fc224216ee66a7
Tyler Young <tyler.young@impromptu.ninja>
2020-06-30 22:48:49 -0400

    merge vector extensions

--- Fizix/VectorExtensions.cs
+++ Fizix/VectorExtensions.cs
@@ -1,6 +1,7 @@
 using System.Numerics;
 using System.Runtime.CompilerServices;
 using System.Runtime.Intrinsics;
+using System.Runtime.Intrinsics.X86;
 using JetBrains.Annotations;
 
 namespace Fizix {
@@ -9,20 +10,32 @@ namespace Fizix {
   public static class VectorExtensions {
 
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public static ref Vector2 AsVector2(in this Vector64<float> a)
-      => ref Unsafe.As<Vector64<float>, Vector2>(ref Unsafe.AsRef(a));
-
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static ref Vector4 AsVector4(in this Vector128<float> a)
       => ref Unsafe.As<Vector128<float>, Vector4>(ref Unsafe.AsRef(a));
 
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public static ref Vector64<float> AsVector64Float(in this Vector2 a)
-      => ref Unsafe.As<Vector2, Vector64<float>>(ref Unsafe.AsRef(a));
-
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static ref Vector128<float> AsVector128Float(in this Vector4 a)
       => ref Unsafe.As<Vector4, Vector128<float>>(ref Unsafe.AsRef(a));
+
+    public static Vector128<float> ToVector128(this Vector2 v) {
+      var x = Vector128.CreateScalarUnsafe(v.X);
+      if (!Sse.IsSupported)
+        return x.WithElement(1, v.Y);
+
+      var y = Vector128.CreateScalarUnsafe(v.Y);
+      return Sse.UnpackLow(x, y);
+    }
+
+    public static Vector2 ToVector2(this Vector128<float> v) {
+      var x = v.GetElement(0);
+      var y = v.GetElement(1);
+      return new Vector2(x, y);
+    }
+
+    public static void Deconstruct(this Vector2 v, out float x, out float y) {
+      x = v.X;
+      y = v.Y;
+    }
 
   }
 
